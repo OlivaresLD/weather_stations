2e9e63ae62d2ccdb10915c3053af3c005ab44c5e
Luis Olivares <45223906+OlivaresLD@users.noreply.github.com>
2021-07-27 00:07:14 -0500

    funcion_install_packs.R
    ##### AUTOMATIC INSTALLATION AND LIBRARIES UPLOAD WITH R #####
    ##### INSTALACIÓN Y CARGA AUTOMÁTICA DE PAQUETES EN R #####

--- funcion_install_packs.R
+++ funcion_install_packs.R
@@ -1,8 +1,12 @@
+##### AUTOMATIC INSTALLATION AND LIBRARIES UPLOAD WITH R #####
 ##### INSTALACIÓN Y CARGA AUTOMÁTICA DE PAQUETES EN R #####
 paquetes <- function(p){
-  new <- p[!(p %in% installed.packages() )]
-  if (length(new)) install.packages(new)
+  new <- p[!(p %in% installed.packages()[,"Package"])]
+  if (length(new) != 0){install.packages(new)}
   
-  do.call(require, as.list(p))
-  }
+  lapply(as.list(p), library, character.only = TRUE)
 }
+
+# NOT RUN
+# p <- c("raster","dplyr", "sf","ggmap")
+# paquetes(p)
